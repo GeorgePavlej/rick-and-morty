4ab46241c6b9fec1ecdd169ddd4b5d10dcb5208c
GeorgePavlej <georgepavlej@gmail.com>
2023-04-07 19:42:37 +0100

    Add serializers, views and urls

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from random import choice
 
-# Create your views here.
+from django.db.models import QuerySet
+from rest_framework import status, generics
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from characters.models import Character
+from characters.serializers import CharacterSerializer
+
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
+
+from pagination import CharactersListPagination
+
+
+@extend_schema(
+    responses={status.HTTP_200_OK: CharacterSerializer},
+)
+@api_view(["GET"])
+def get_random_character_view(request: Request) -> Response:
+    """Get a random character from Rick and Morty world"""
+    pks = Character.objects.values_list("pk", flat=True)
+    random_pk = choice(pks)
+    random_character = Character.objects.get(pk=random_pk)
+    serializer = CharacterSerializer(random_character)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+    pagination_class = CharactersListPagination
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+    @extend_schema(
+        # extra parameters added to the schema
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter by characters name",
+                required=False,
+                type=str,
+            ),
+        ],
+    )
+    def get(self, request):
+        """Filter characters by name"""
+        return super().get(request)
